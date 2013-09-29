8cf4ed896fe93de9e2d5a83577c8661e62553852
SoHotSoup <3.14wee@gmail.com>
2013-09-29 22:57:46 +0200

    styles and content

---
+++ app/scripts/slides.js
@@ -0,0 +1,9 @@
+window.slides = [
+	{ title: 'Example Presentation' },
+	{ title: 'Life of bugs' },
+	{ title: 'Slonovi jedu travu, u travi žive mravi, a žirafe brste lišće, dok na njihovim njuškana kakaju muhe' },
+	{ image: 'http://www.jutarnji.hr/multimedia/archive/00369/mate01_369173S1.jpg' },
+	{ title: 'spreman za koncerat', bullets: ['pjesma prva', 'pjesma druga', 'pjesma treca'] },
+	{ quote: 'Ti si pjesma moje duše...', cite: 'MMK'}
+
+]
